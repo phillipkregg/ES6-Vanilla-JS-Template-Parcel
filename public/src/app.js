bba990015f705b4bbcef3cb8003425fec5fe6165
Phil Lackey <plackey@costar.com>
2021-01-30 14:45:57 -0600

    Initial commit - complete dev environment for getting started with ES6 and Parcel bundler

---
+++ public/src/app.js
@@ -0,0 +1,12 @@
+import 'regenerator-runtime/runtime.js';
+import './scss/app.scss';
+import Header from './components/Header';
+import User from './components/User';
+
+const app = async () => {
+  document.getElementById('header').innerHTML = Header();
+  document.getElementById('user').innerHTML = await User();
+};
+
+// Init app
+app();
---
+++ public/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const template = `
+        <header>
+            <h1>My Parcel App</h1>
+            <p>This is a boilerplate for simple js workflow</p>
+        </header>
+    `;
+
+  return template;
+};
+
+export default Header;
